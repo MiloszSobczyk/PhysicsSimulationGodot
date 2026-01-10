2f72890c1d2d6b6b2ab9fd8c10311858d722bf80
MiloszSobczyk <m.sobczyk.official@gmail.com>
2026-01-10 21:36:54 +0100

    Expand SPinningCube and add basic physics

--- scripts/SpinningCube.cs
+++ scripts/SpinningCube.cs
@@ -2,15 +2,46 @@ using Godot;
 
 namespace PhysicsSimulationGodot.scripts;
 
+[Tool]
 public partial class SpinningCube : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    private RigidBody3D _cube;
+    private float _tiltAngle = 0.0f;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    [Export(PropertyHint.Range, "0, 90")]
+    public float TiltAngle
+    {
+        get => _tiltAngle;
+        set
+        {
+            _tiltAngle = value;
+            ApplyTransform();
+        }
+    }
+
+    public override void _Ready()
+    {
+        _cube = GetNode<RigidBody3D>("Cube");
+        ApplyTransform();
+    }
+
+    private void ApplyTransform()
+    {
+        if (_cube == null) return;
+
+        Vector3 localDiagonal = new Vector3(1.0f, 1.0f, 1.0f).Normalized();
+        Quaternion alignQuat = new Quaternion(localDiagonal, Vector3.Up);
+
+        float tiltRadians = Mathf.DegToRad(_tiltAngle);
+        Quaternion tiltQuat = new Quaternion(Vector3.Right, tiltRadians);
+        
+        Quaternion finalRotation = tiltQuat * alignQuat;
+        _cube.Quaternion = finalRotation;
+
+        Vector3 bottomCorner = new Vector3(-0.5f, -0.5f, -0.5f);
+
+        Vector3 currentCornerOffset = finalRotation * bottomCorner;
+
+        _cube.Position = -currentCornerOffset;
+    }
 }
