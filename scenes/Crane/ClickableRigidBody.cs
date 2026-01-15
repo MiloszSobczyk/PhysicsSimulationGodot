35206a8fc57ad7178ed4a0c8e78c5796de101489
Portalion <77tobik77@gmail.com>
2026-01-15 19:11:20 +0100

    add crane

--- scenes/Crane/ClickableRigidBody.cs
+++ scenes/Crane/ClickableRigidBody.cs
@@ -4,21 +4,57 @@ using System;
 [GlobalClass]
 public partial class ClickableRigidBody : RigidBody3D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+	private bool isBeingGrabbed = false;
+    Vector3 grabOffset;
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-	{
-	}
-
-	public void HandleInput(Camera3D camera, InputEvent @event, Vector3 eventPosition, Vector3 normal, int shapeIndex)
-	{
-		if(Input.IsActionPressed("AddImpulse"))
+    public override void _PhysicsProcess(double delta)
+    {
+        if (!isBeingGrabbed)
 		{
-			ApplyImpulse((eventPosition - camera.Position).Normalized(), eventPosition);
+			return;
 		}
-	}
+
+        Vector3 target = GetMouseWorldPoint() + grabOffset;
+        Vector3 error = target - GlobalPosition;
+
+        Vector3 force = error * 90f - LinearVelocity * 14f;
+        ApplyCentralForce(force * Mass);
+    }
+
+    Vector3 GetMouseWorldPoint()
+    {
+        var cam = GetViewport().GetCamera3D();
+        var m = GetViewport().GetMousePosition();
+
+        var from = cam.ProjectRayOrigin(m);
+        var dir = cam.ProjectRayNormal(m);
+
+        var q = PhysicsRayQueryParameters3D.Create(from, from + dir * 1000);
+        var hit = GetWorld3D().DirectSpaceState.IntersectRay(q);
+
+        return hit.Count > 0 ? (Vector3)hit["position"] : GlobalPosition;
+    }
+
+    public void HandleInput(Camera3D camera, InputEvent @event, Vector3 position, Vector3 normal, int shapeIndex)
+	{
+		if (Input.IsActionPressed("AddImpulse"))
+		{
+			Vector3 dir = -normal;
+			float strength = 6f * Mass;
+			
+            ApplyImpulse(dir * strength, position - GlobalPosition);
+            GetViewport().SetInputAsHandled();
+        }
+
+		if (!isBeingGrabbed && Input.IsActionPressed("DragObject"))
+		{
+			isBeingGrabbed = true;
+            grabOffset = GlobalPosition - position;
+		}
+
+		if (Input.IsActionJustReleased("DragObject"))
+		{
+			isBeingGrabbed = false;
+        }
+    }
 }
